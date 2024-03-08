bc679a0e9af6c50619bec10eb84b1236d1845437
Davide Agostini <d.agostini@synapseslab.com>
2024-03-08 15:58:30 +0100

    add github actions

---
+++ .github/workflows/generate_docs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# The website is built using MkDocs with the Material theme.
+# https://squidfunk.github.io/mkdocs-material/
+# It requires Python to run.
+# Install the packages with the following command:
+# pip install mkdocs mkdocs-material mkdocs-redirects
+
+set -ex
+
+# Build the site locally
+cd ./android-docs
+python3 -m venv venv
+source venv/bin/activate
+pip3 install mkdocs-material
+mkdocs build
